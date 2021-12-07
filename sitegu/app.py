01aeeaed34c57cd5713bd5cea6e5600a476e6a5c
sabino_gs <sabino.gustavo.gs@gmail.com>
2021-12-07 10:29:21 -0300

    Architecture defined and blueprints added.

--- sitegu/app.py
+++ sitegu/app.py
@@ -1,7 +1,10 @@
-from flask import Flask 
+from flask import Flask
+from sitegu.ext import site
+
 
 def create_app():
     app = Flask(__name__)
+    site.init_app(app) 
     return app
     
 app = create_app()
