f358677a232c0da1498f75cddb8a0157b0b66f37
Sabino <core.editor>
2021-12-20 10:48:03 -0300

    templates created

--- sitegu/ext/site/main.py
+++ sitegu/ext/site/main.py
@@ -7,6 +7,11 @@ bp = Blueprint("site", __name__)
 @bp.route("/")
 def index():
     return render_template(
-        'index.html',
-            name = request.args['name']
+        'index.html'
+    )
+
+@bp.route("/sobre")
+def about():
+    return render_template(
+        'about.html'
     )
